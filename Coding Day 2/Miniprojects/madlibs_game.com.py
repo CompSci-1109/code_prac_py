811a9e6dcb5b464ab47cd331eecd2da88d3fc10b
Compsci-1109 <artismagnaeprincipiamathematic@gmail.com>
2025-08-11 11:36:07 +0530

    new commits

---
+++ Coding Day 2/Miniprojects/madlibs_game.com.py
@@ -0,0 +1,13 @@
+#madlibs game in python , where we complete a story using words guessed by the user.
+
+adjective1= str(input("Enter an adjective(description):"))
+noun1=str(input("Enter a noun: "))
+noun2=str(input("Enter 2nd noun: "))
+noun3=str(input("Enter a 3rd Noun: "))
+noun4=str(input("Enter the last noun: "))
+
+print("I once had a weird dream.")
+print(f"In the dream, I once went to a {adjective1} park")
+print(f"I saw {noun1},{noun2} & {noun3} there.")
+print(f"Then I saw a cage. In which I saw {noun4}")
+print(f"I was so happy to see {noun4} in the cage! HAHAHA!🤣🤣🤣🤣")
